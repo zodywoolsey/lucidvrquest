f0d12e2df9176d7f381b4977fcaea27569b4374d
zodywoolsey <zodywoolsey@gmail.com>
2020-01-26 22:23:11 -0600

    updated a ton of stuff

--- scripts_gd/menuWorld.gd
+++ scripts_gd/menuWorld.gd
@@ -9,7 +9,7 @@ var floorTiles = []
 var litTileScene
 
 var tmpTile
-var tileMoveFlag = false
+var tileMoveFlag = true
 
 var i = 0
 var a = -1
@@ -24,42 +24,31 @@ func _ready():
 	# 	tmpTile.translate(Vector3(-.5+i,0,0))
 	# 	floorTiles.append(tmpTile)
 
-	# for i in range(9):
-	# 	for a in range(9):
-	# 		tmpTile = floorTileScene.instance()
-	# 		add_child(tmpTile)
-	# 		# tmpTile.translate(Vector3(((a)-4), -200+i, -i+5))
-	# 		tmpTile.global_transform.origin = Vector3(((a)-4), -50+i+a, -i+5)
-	# 		floorTiles.append(tmpTile)
+	for i in range(50):
+		for a in range(50):
+			tmpTile = floorTileScene.instance()
+			#tmpTile.translate(Vector3(((a)-4), -200+i, -i+5))
+			tmpTile.global_transform.origin = Vector3(((a)-25), -100+i+a, -i+25)
+			add_child(tmpTile)
+			floorTiles.append(tmpTile)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(delta):
-	if len(floorTiles) < 100:
-		print('less than 100')
-		if i%10 == 0:
-			print('a+')
-			a += 1
-		tmpTile = floorTileScene.instance()
-		add_child(tmpTile)
-		# tmpTile.translate(Vector3(((a)-4), -200+i, -i+5))
-		tmpTile.global_transform.origin = Vector3(((a)-4), -50+i+a, -i+5)
-		floorTiles.append(tmpTile)
-	else:
-		tileMoveFlag = true
-
+func _process(delta):
 	if tileMoveFlag == true:
 		checkAndMoveTiles()
 
 func checkAndMoveTiles():
 	tileMoveFlag = false
 	for tile in floorTiles:
-		if tile.global_transform.origin.y < 0:
-			# tile.translate(Vector3(0,1,0))
-			tile.global_transform.origin.y += .1
-			if (tile.global_transform.origin.y < 1 && tile.global_transform.origin.y > 0 ):
-				tile.global_transform.origin.y = 0
-			else:
+		var tmpOrig = tile.global_transform.origin
+		if tile.global_transform.origin.y != 0:
+			# tile.translate(Vector3(0,1+(tile.global_transform.origin.y/4),0))
+			tile.global_transform.origin = Vector3(tmpOrig.x,tmpOrig.y/1.01,tmpOrig.z)
+			if tile.global_transform.origin.y > -1 && tile.global_transform.origin.y != 0:
+				tile.global_transform.origin = Vector3(tmpOrig.x,tmpOrig.y+.01,tmpOrig.z)
+				# tile.global_transform.origin = Vector3( tile.global_transform.origin.x, 0, tile.global_transform.origin.z )
+			if tile.global_transform.origin.y > 0:
+				tile.global_transform.origin = Vector3( tile.global_transform.origin.x, 0, tile.global_transform.origin.z )
+			if tile.global_transform.origin.y != 0:
 				tileMoveFlag = true
-		# else:
-			#print(tile.global_transform.origin)
--- scripts_gd/rightHand.gd
+++ scripts_gd/rightHand.gd
@@ -6,6 +6,8 @@ var handBody
 var handRay
 var grabShader
 
+var otherHandGrab
+
 var grabDown = false
 var triggerDown = false
 
@@ -41,10 +43,11 @@ var grabbedObjectOrigin
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	handGrab = findNode('rightHandGrab')
+	otherHandGrab = findNode('leftHandGrab')
 	handArea = findNode('rightHandArea')
 	handBody = findNode('rightHandBody')
 	handRay = findNode('rightHandRay')
-	grabShader = findNode('grabShaderMesh').get_material_override()
+	grabShader = load('res://handGrabMaterial.tres')
 
 
 func _physics_process(delta):
@@ -53,11 +56,20 @@ func _physics_process(delta):
 	if triggerDown && grabDown && !grabbed:
 		grabRigidGrabbable()
 		pullRigidGrabbable()
-	if grabbedObject && grabbed:
+	if (grabbedObject && grabbed):
+		var isStaticCollided = false
+		collides = handArea.get_overlapping_bodies()
+		for col in collides:
+			if col.get_class() == "StaticBody":
+				isStaticCollided = true
+				break
 		grabbedObjectOrigin = grabbedObject.global_transform.origin
-		if ( sqrt( pow(grabbedObjectOrigin.x-global_transform.origin.x,2)+pow(grabbedObjectOrigin.y-global_transform.origin.y,2)+pow(grabbedObjectOrigin.z-global_transform.origin.z,2) ) > .2 ):
+		if isStaticCollided && ( sqrt( pow(grabbedObjectOrigin.x-global_transform.origin.x,2)+pow(grabbedObjectOrigin.y-global_transform.origin.y,2)+pow(grabbedObjectOrigin.z-global_transform.origin.z,2) ) > .1 ):
 			handGrab.set_node_b('')
 			grabbedObject = null
+	if otherHandGrab.get_node_b() == handGrab.get_node_b():
+		handGrab.set_node_b('')
+		grabbedObject = null
 	if grabbedObject && !grabbed:
 		handGrab.set_node_b("")
 		grabbedObject = null
@@ -66,11 +78,15 @@ func _physics_process(delta):
 			
 func _on_rightHand_button_pressed(button):
 	if button == 2:
-		grabDown = true;
+		grabDown = true
+		if rayCollidedNodeMesh:
+			rayCollidedNodeMesh.material_override = null
+			rayCollidedNodeMesh = null
+			tmpMat = null
 	if button == 15 && !grabbed:
 		triggerDown = true
 		rayOn = true
-		handRay.set_enabled(true)
+		handRay.enabled = true
 		handRay.show()
 
 func _on_rightHand_button_release(button):
@@ -88,7 +104,7 @@ func _on_rightHand_button_release(button):
 	if button == 15:
 		triggerDown = false
 		rayOn = false
-		handRay.set_enabled(false)
+		handRay.enabled = false
 		handRay.hide()
 		if rayCollidedNode:
 			rayCollidedNode.set_gravity_scale(gravtmp)
@@ -98,7 +114,7 @@ func _on_rightHand_button_release(button):
 			gravtmp = null
 			damptmp = null
 		if rayCollidedNodeMesh:
-			rayCollidedNodeMesh.set_surface_material(0, tmpMat)
+			rayCollidedNodeMesh.material_override = null
 			rayCollidedNodeMesh = null
 			tmpMat = null
 
@@ -120,8 +136,8 @@ func grabRigidGrabbable():
 				grabbedObject.global_transform = handBody.global_transform
 				handGrab.set_node_b(col.get_path())
 				grabbed = true
-				if handRay.is_enabled() == true:
-					handRay.set_enabled(false)
+				if handRay.enabled == true:
+					handRay.enabled = false
 					handRay.hide()
 					if rayCollidedtmp:
 						rayCollidedtmp.set_gravity_scale(gravtmp)
@@ -130,23 +146,14 @@ func grabRigidGrabbable():
 						rayCollidedNode = null
 						damptmp = null
 						gravtmp = null
-					if rayCollidedNodeMesh:
-						rayCollidedNodeMesh.set_surface_material(0, tmpMat)
-						rayCollidedNodeMesh = null
-						tmpMat = null
 					rayOn = false
 				break
 
 func pullRigidGrabbable():
+	applyGrabShader()
 	if handRay.is_colliding() && !rayCollidedNode && handRay.get_collider().get_class() == "RigidBody" && checkNodeGroups(handRay.get_collider(), 'grabbable'):
 		rayCollidedNode = get_node(handRay.get_collider().get_path())
 		rayCollidedNodeMesh = rayCollidedNode.find_node('MeshInstance',true,true)
-		if rayCollidedNodeMesh:
-			if rayCollidedNodeMesh.get_surface_material(0) == null:
-				rayCollidedNodeMesh.set_surface_material(0, grabShader)
-			elif rayCollidedNodeMesh.get_surface_material(0).get_class() == "SpatialMaterial":
-				tmpMat = rayCollidedNodeMesh.get_surface_material(0)
-				rayCollidedNodeMesh.get_surface_material(0).flags_no_depth_test = true
 		pullInterval = 0
 	if grabDown && rayCollidedNode:
 		if pullInterval < 72:
@@ -196,45 +203,18 @@ func checkNodeNameGroups(nodeName, groupName):
 func applyGrabShader():
 	if rayCollidedNodeMesh && handRay.is_colliding():
 		if rayCollidedNodeMesh != get_node(handRay.get_collider().get_path()).find_node('MeshInstance',true,true):
-			rayCollidedNodeMesh.set_surface_material(0, null)
+			rayCollidedNodeMesh.material_override = null
 			rayCollidedNodeMesh = null
 			tmpMat = null
 	elif rayCollidedNodeMesh && handRay.is_colliding() == false:
-		rayCollidedNodeMesh.set_surface_material(0, null)
+		rayCollidedNodeMesh.material_override = null
 		rayCollidedNodeMesh = null
 		tmpMat = null
 	if handRay.is_colliding() && !rayCollidedNode && handRay.get_collider().get_class() == "RigidBody" && checkNodeGroups(handRay.get_collider(), 'grabbable'):
 		rayCollidedNodeMesh = get_node(handRay.get_collider().get_path()).find_node('MeshInstance',true,true)
 		if rayCollidedNodeMesh:
-			if rayCollidedNodeMesh.get_surface_material(0) == null:
-				rayCollidedNodeMesh.set_surface_material(0, grabShader)
-			elif rayCollidedNodeMesh.get_surface_material(0).get_class() == "SpatialMaterial":
-				tmpMat = rayCollidedNodeMesh.get_surface_material(0)
-				rayCollidedNodeMesh.get_surface_material(0).flags_no_depth_test = true
+			rayCollidedNodeMesh.material_override = grabShader
+			rayCollidedNodeMesh.material_override.next_pass = rayCollidedNodeMesh.get_surface_material(0)
+			
 
 
-#		pullBackup:
-#if handRay.is_colliding() && grabDown:
-# if handRay.get_collider().get_class() == "RigidBody":
-# 	rayCollidedNode = get_node(handRay.get_collider().get_path())
-# 	if !rayCollidedtmp || !gravtmp:
-# 		rayCollidedtmp = rayCollidedNode
-# 		gravtmp = rayCollidedtmp.get_gravity_scale()
-# 	if rayCollidedNode != rayCollidedtmp:
-# 		rayCollidedtmp.set_gravity_scale(gravtmp)
-# 		rayCollidedtmp = rayCollidedNode
-# 		gravtmp = rayCollidedtmp.get_gravity_scale()
-# 	if checkNodeGroups(rayCollidedNode,'grabbable'):
-# 		forceStrength = rayCollidedNode.get_mass()*50
-# 		rayCollidedNode.set_gravity_scale(0)
-# 		rayCollidedNodeOrigin = rayCollidedNode.get_global_transform().origin
-# 		handOrigin = get_global_transform().origin
-# 		towardVector = handOrigin-rayCollidedNodeOrigin
-# 		if abs(towardVector[0]) > abs(towardVector[1]) && abs(towardVector[0]) > abs(towardVector[2]):
-# 			forceScaleFactor = abs(forceStrength/towardVector[0])
-# 		elif abs(towardVector[1]) > abs(towardVector[0]) && abs(towardVector[1]) > abs(towardVector[2]):
-# 			forceScaleFactor = abs(forceStrength/towardVector[1])
-# 		elif abs(towardVector[2]) > abs(towardVector[1]) && abs(towardVector[2]) > abs(towardVector[0]):
-# 			forceScaleFactor = abs(forceStrength/towardVector[2])
-# 		towardVector = towardVector*forceScaleFactor
-# 		rayCollidedNode.add_force( towardVector, Vector3(0,0,0))
