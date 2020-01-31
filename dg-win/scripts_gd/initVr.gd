6044b34c5697fd4eb6b9680fc0d9acab71bc699a
Zody <zodywoolsey@users.noreply.github.com>
2020-01-31 08:51:33 -0600

    added test proj dg-win

---
+++ dg-win/scripts_gd/initVr.gd
@@ -0,0 +1,52 @@
+extends Spatial
+
+# onready var ovr_init_config = preload("res://addons/godot_ovrmobile/OvrInitConfig.gdns").new()
+# onready var ovr_performance = preload("res://addons/godot_ovrmobile/OvrPerformance.gdns").new()
+var ovr_init_config
+var ovr_performance
+
+var perform_runtime_config = false
+var arServ = ''
+
+var settingsFile
+var interface = null
+
+func _ready():
+	interface = ARVRServer.find_interface("OVRMobile")
+	arServ = 'ovrMobile'
+	settingsFile = File.new()
+	if not settingsFile.file_exists('res://settings.txt'):
+		settingsFile.open('res://settings.txt', File.WRITE)
+	if settingsFile.open("res://settings.txt",File.READ) != 0:
+		print('Error, will crash')
+	var settingsData = ''
+	settingsData = settingsFile.get_line()
+	print(settingsData)
+	if settingsData == 'ovr':
+		interface = ARVRServer.find_interface('OpenVR')
+		arServ = 'ovr'
+		Engine.iterations_per_second = 90
+	elif settingsData == 'oculus':
+		interface = ARVRServer.find_interface('Oculus')
+		arServ = 'oculus'
+		Engine.iterations_per_second = 80
+	if interface:
+		if arServ == 'ovrMobile':
+			ovr_init_config = load('res://addons/godot_ovrmobile/OvrInitConfig.gdns').new()
+			ovr_performance = load('res://addons/godot_ovrmobile/OvrPerformance.gdns').new()
+			ovr_init_config.set_render_target_size_multiplier(1)
+			ovr_performance.set_clock_levels(1, 1)
+			ovr_performance.set_extra_latency_mode(1)
+			Engine.iterations_per_second = 72
+			
+		if interface.initialize():
+			get_viewport().arvr = true
+			OS.vsync_enabled = false
+			if arServ == 'ovr':
+				get_viewport().keep_3d_linear = true
+
+# func _process(_delta):
+# 	if !perform_runtime_config:
+# 		ovr_performance.set_clock_levels(1, 1)
+# 		ovr_performance.set_extra_latency_mode(1)
+# 		perform_runtime_config = true
---
+++ dg-win/scripts_gd/rightHand.gd
@@ -0,0 +1,220 @@
+extends ARVRController
+
+var handGrab
+var handArea
+var handBody
+var handRay
+var grabShader
+
+var otherHandGrab
+
+var grabDown = false
+var triggerDown = false
+
+var rayOn = false
+
+var rayCollidedNode
+var rayCollidedNodeMesh = null
+var rayCollidedNodeOrigin
+var handOrigin
+var towardVector
+var forceScaleFactor
+var forceStrength = 50
+
+var gravtmp = null
+var damptmp = null
+var rayCollidedtmp = null
+var tmpMat = null
+var pullInterval = 0
+
+var body
+var isCollided
+var collides
+var grabbed = false
+var isGrabbable
+var grabbedObject = null
+var grabbedObjectOrigin
+
+# var velStartTransformOrigin
+# var velEndTransformOrigin
+# var vel
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	handGrab = findNode('rightHandGrab')
+	otherHandGrab = findNode('leftHandGrab')
+	handArea = findNode('rightHandArea')
+	handBody = findNode('rightHandBody')
+	handRay = findNode('rightHandRay')
+	grabShader = load('res://handGrabMaterial.tres')
+
+
+func _physics_process(delta):
+	if grabDown:
+		grabRigidGrabbable()
+	if triggerDown && grabDown && !grabbed:
+		grabRigidGrabbable()
+		pullRigidGrabbable()
+	if (grabbedObject && grabbed):
+		var isStaticCollided = false
+		collides = handArea.get_overlapping_bodies()
+		for col in collides:
+			if col.get_class() == "StaticBody":
+				isStaticCollided = true
+				break
+		grabbedObjectOrigin = grabbedObject.global_transform.origin
+		if isStaticCollided && ( sqrt( pow(grabbedObjectOrigin.x-global_transform.origin.x,2)+pow(grabbedObjectOrigin.y-global_transform.origin.y,2)+pow(grabbedObjectOrigin.z-global_transform.origin.z,2) ) > .1 ):
+			handGrab.set_node_b('')
+			grabbedObject = null
+	if otherHandGrab.get_node_b() == handGrab.get_node_b():
+		handGrab.set_node_b('')
+		grabbedObject = null
+	if grabbedObject && !grabbed:
+		handGrab.set_node_b("")
+		grabbedObject = null
+	if triggerDown:
+		applyGrabShader()
+			
+func _on_rightHand_button_pressed(button):
+	if button == 2:
+		grabDown = true
+		if rayCollidedNodeMesh:
+			rayCollidedNodeMesh.material_override = null
+			rayCollidedNodeMesh = null
+			tmpMat = null
+	if button == 15 && !grabbed:
+		triggerDown = true
+		rayOn = true
+		handRay.enabled = true
+		handRay.show()
+
+func _on_rightHand_button_release(button):
+	if button == 2:
+		grabDown = false
+		if grabbed:
+			grabbed = false
+		if rayCollidedNode:
+			rayCollidedNode.set_gravity_scale(gravtmp)
+			rayCollidedNode.set_linear_damp(damptmp)
+			rayCollidedtmp = null
+			rayCollidedNode = null
+			gravtmp = null
+			damptmp = null
+	if button == 15:
+		triggerDown = false
+		rayOn = false
+		handRay.enabled = false
+		handRay.hide()
+		if rayCollidedNode:
+			rayCollidedNode.set_gravity_scale(gravtmp)
+			rayCollidedNode.set_linear_damp(damptmp)
+			rayCollidedtmp = null
+			rayCollidedNode = null
+			gravtmp = null
+			damptmp = null
+		if rayCollidedNodeMesh:
+			rayCollidedNodeMesh.material_override = null
+			rayCollidedNodeMesh = null
+			tmpMat = null
+
+
+func _on_rightHandArea_body_entered(body):
+	isCollided = true
+
+func _on_rightHandArea_body_exited(body):
+	isCollided = false
+
+func grabRigidGrabbable():
+	if isCollided && !grabbed:
+		collides = handArea.get_overlapping_bodies()
+		for col in collides:
+			isGrabbable = false
+			isGrabbable = checkNodeGroups(col, 'grabbable')
+			if col.get_class() == "RigidBody" && isGrabbable:
+				grabbedObject = col
+				grabbedObject.global_transform = handBody.global_transform
+				handGrab.set_node_b(col.get_path())
+				grabbed = true
+				if handRay.enabled == true:
+					handRay.enabled = false
+					handRay.hide()
+					if rayCollidedtmp:
+						rayCollidedtmp.set_gravity_scale(gravtmp)
+						rayCollidedtmp.set_linear_damp(damptmp)
+						rayCollidedtmp = null
+						rayCollidedNode = null
+						damptmp = null
+						gravtmp = null
+					rayOn = false
+				break
+
+func pullRigidGrabbable():
+	applyGrabShader()
+	if handRay.is_colliding() && !rayCollidedNode && handRay.get_collider().get_class() == "RigidBody" && checkNodeGroups(handRay.get_collider(), 'grabbable'):
+		rayCollidedNode = get_node(handRay.get_collider().get_path())
+		rayCollidedNodeMesh = rayCollidedNode.find_node('MeshInstance',true,true)
+		pullInterval = 0
+	if grabDown && rayCollidedNode:
+		if pullInterval < 72:
+			pullInterval = pullInterval+1
+		else:
+			pullInterval = 0
+			rayCollidedNode.global_transform = global_transform
+		if !rayCollidedtmp || !gravtmp || !damptmp:
+			rayCollidedtmp = rayCollidedNode
+			gravtmp = rayCollidedtmp.get_gravity_scale()
+			damptmp = rayCollidedtmp.get_linear_damp()
+		if rayCollidedNode != rayCollidedtmp:
+			rayCollidedtmp.set_gravity_scale(gravtmp)
+			rayCollidedtmp = rayCollidedNode
+			gravtmp = rayCollidedtmp.get_gravity_scale()
+			damptmp = rayCollidedtmp.get_linear_damp()
+		forceStrength = rayCollidedNode.get_mass()*50
+		rayCollidedNode.set_gravity_scale(0)
+		rayCollidedNodeOrigin = rayCollidedNode.get_global_transform().origin
+		handOrigin = get_global_transform().origin
+		towardVector = handOrigin-rayCollidedNodeOrigin
+		if abs(towardVector[0]) > abs(towardVector[1]) && abs(towardVector[0]) > abs(towardVector[2]):
+			forceScaleFactor = abs(forceStrength/towardVector[0])
+		elif abs(towardVector[1]) > abs(towardVector[0]) && abs(towardVector[1]) > abs(towardVector[2]):
+			forceScaleFactor = abs(forceStrength/towardVector[1])
+		elif abs(towardVector[2]) > abs(towardVector[1]) && abs(towardVector[2]) > abs(towardVector[0]):
+			forceScaleFactor = abs(forceStrength/towardVector[2])
+		towardVector = towardVector*forceScaleFactor
+		rayCollidedNode.add_force( towardVector, Vector3(0,0,0))
+
+
+func findNode(nodeName):
+	return get_node('/root').find_node(nodeName, true, false)
+
+func checkNodeGroups(node, groupName):
+	for group in node.get_groups():
+		if group == groupName:
+			return true
+	return false
+func checkNodeNameGroups(nodeName, groupName):
+	for group in findNode(nodeName).get_groups():
+		if group == groupName:
+			return true
+	return false
+
+
+func applyGrabShader():
+	if rayCollidedNodeMesh && handRay.is_colliding():
+		if rayCollidedNodeMesh != get_node(handRay.get_collider().get_path()).find_node('MeshInstance',true,true):
+			rayCollidedNodeMesh.material_override = null
+			rayCollidedNodeMesh = null
+			tmpMat = null
+	elif rayCollidedNodeMesh && handRay.is_colliding() == false:
+		rayCollidedNodeMesh.material_override = null
+		rayCollidedNodeMesh = null
+		tmpMat = null
+	if handRay.is_colliding() && !rayCollidedNode && handRay.get_collider().get_class() == "RigidBody" && checkNodeGroups(handRay.get_collider(), 'grabbable'):
+		rayCollidedNodeMesh = get_node(handRay.get_collider().get_path()).find_node('MeshInstance',true,true)
+		if rayCollidedNodeMesh:
+			rayCollidedNodeMesh.material_override = grabShader
+			rayCollidedNodeMesh.material_override.next_pass = rayCollidedNodeMesh.get_surface_material(0)
+			
+
+
