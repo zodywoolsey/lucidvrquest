e7c43b1369a242b8cb18e8735d2a56b0e26746bb
Zody <zodywoolsey@users.noreply.github.com>
2020-09-21 13:08:53 -0500

    working on enemy code

--- scenes/NavRigidBody.gd
+++ scenes/NavRigidBody.gd
@@ -1,14 +1,12 @@
-extends RigidBody
+extends MeshInstance
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+onready var rigidbody = get_node("RigidBody")
+onready var collisionshape = get_node("RigidBody/CollisionShape")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	CollisionShape.new().shape = create_convex_collision()
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
